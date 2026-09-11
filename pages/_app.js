fa6456066b16b7f9b33c2cd07e233ae0f06daf51
Matrixxx17 <rohansuryavanshi0017@gmail.com>
2026-09-11 17:47:33 +0530

    first commit till home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
-import "@/styles/globals.css";
+import Link from "next/link";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header>
+        <nav>
+          <Link href="/">
+            HDFC Life Advisor Portal
+          </Link>
+
+          <div>
+            <Link href="/">Home</Link>
+            <Link href="/policies">Policies</Link>
+            <Link href="/claims">Claims</Link>
+            <Link href="/claims/new">File Claim</Link>
+            <Link href="/desk">Advisor Desk</Link>
+          </div>
+        </nav>
+      </header>
+
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
